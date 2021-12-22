81607d71cf5cb76c7a6db6862720ac85a6dd0435
ankit <devaniankit1990@gmail.com>
2021-12-22 18:20:17 +0530

    transition added

--- src/Components/Intro.jsx
+++ src/Components/Intro.jsx
@@ -1,7 +1,8 @@
 import "./Intro.css"
+import Me from "../images/me.jpg";
 const Intro = () => {
     return (
-        <div>
+        <div className="i">
            <div className="i-left">
            <div className="i-left-wrapper">
                <h2 className="i-intro"> Hello, My name is </h2>
@@ -9,14 +10,17 @@ const Intro = () => {
                <div className="i-title">
                    <div className="i-title-wrapper">
                        <div className="i-title-item">Web Developer</div>
-                       <div className="i-title-item">Tech Enthusiast</div>
-                       <div className="i-title-item">React Developer</div>     
+                       <div className="i-title-item">CSE Undergrad</div>  
+                       <div className="i-title-item">Tech Enthusiast</div>  
                    </div>
                </div>
+               <div className="i-desc">
+              I am a third year CSE Undergrad and a Web Dev Enthusiast with a keen interest in MERN Stack.
+          </div>
            </div>
            </div>
            <div className="i-right">
-
+           <img src={Me} alt="" className="i-img" />
            </div>
 
            </div>
